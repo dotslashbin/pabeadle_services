bc205d3682d97acb2f9c6982357a07101ea7ee8d
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-20 13:03:38 +0800

    Implemented a working "add"

--- srvsapp/gin.go
+++ srvsapp/gin.go
@@ -1,11 +1,20 @@
 package srvsapp
 
 import (
+	"github.com/dotslashbin/pabeadle_services/srvsdatabases/mongodbrepo"
 	"github.com/gin-gonic/gin"
 )
 
 func Gin() *gin.Engine {
 	ginFramework := gin.Default()
+
+	// Database
+	db := mongodbrepo.MongoDBRepo{}
+	db.InitDB()
+
+	ginFramework.Use(func(context *gin.Context) {
+		context.Set("db", &db)
+	})
 
 	InitializeRoutes(ginFramework)
 
